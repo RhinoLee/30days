e1785177d2f0e50168aa152f804cd0a1f4dc850e
Rhino Lee <picklerhino@gmail.com>
2024-08-18 16:06:11 +0800

    throttle_01_簡易版實作_rejectOnCancel

--- throttle_01/throttle.js
+++ throttle_01/throttle.js
@@ -8,36 +8,54 @@ function clickHandler () {
 const createFilterWrapper = (filter, fn) => {
   function wrapper(...args) {
     // 這邊的 this 源頭會是 clickHandlerThrottled
-    filter(() => fn.apply(this, args), { fn, this: this, args })
+    return new Promise((resolve, reject) => {
+      Promise.resolve(filter(() => fn.apply(this, args), { fn, this: this, args }))
+        .then(resolve)
+        .catch(reject)
+    })
   };
 
   return wrapper;
 }
 
 const throttle = (...args) => {
-  const [delay, trailing = true, leading = true] = args
+  const [delay, trailing = true, leading = true, rejectOnCancel = false] = args
   let lastExec = 0;
   let timer = null;
   let isLeading = true;
+  let lastRejector = () => {};
+  let lastValue;
 
-  return function (invoke) {
+  return function (_invoke) {
     const duration = Date.now() - lastExec;
 
-    if (timer) {
-      clearTimeout(timer);
-      timer = null;
+    const invoke = () => {
+      return lastValue = _invoke()
     }
+
+    const clear = () => {
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+        lastRejector();
+        lastRejector = () => {};
+      }
+    }
+
+    clear();
 
     if (duration >= delay && (leading || !isLeading)) {
       lastExec = Date.now();
       invoke();
     } else if (trailing) {
-      timer = setTimeout(() => {
-        lastExec = Date.now();
-        invoke();
-        clearTimeout(timer);
-        timer = null;
-      }, Math.max(0, delay - duration))
+      lastValue = new Promise((resolve, reject) => {
+        lastRejector = rejectOnCancel ? reject : resolve
+        timer = setTimeout(() => {
+          lastExec = Date.now();
+          resolve(invoke());
+          clear();
+        }, Math.max(0, delay - duration))
+      })
     }
 
     if (!leading && !timer)  {
@@ -47,6 +65,8 @@ const throttle = (...args) => {
     }
 
     isLeading = false;
+
+    return lastValue;
   }
 };
 
@@ -55,13 +75,20 @@ const useThrottle = (
   delay = 200,
   trailing = false,
   leading = true,
+  rejectOnCancel = false,
 ) => {
   return createFilterWrapper(
-    throttle(delay, trailing, leading),
+    throttle(delay, trailing, leading, rejectOnCancel),
     fn,
   )
 }
 
-const clickHandlerThrottled = useThrottle(clickHandler, 2000, true, false);
+const clickHandlerThrottled = useThrottle(clickHandler, 2000, true, true, true);
 
-clickEl.addEventListener("click", clickHandlerThrottled);
+clickEl.addEventListener("click", function (){
+  clickHandlerThrottled.apply(this).then(() => {
+    console.log("clickHandlerThrottled resolve");
+  }).catch(() => {
+    console.log("clickHandlerThrottled reject");
+  })
+});
